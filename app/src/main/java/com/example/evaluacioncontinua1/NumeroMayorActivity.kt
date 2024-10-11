0a982a5549c3c7bbf211892e6f7522c7b8c1e3a0
AlexCarbajoG <carbajoalexander61@gmail.com>
2024-10-10 22:32:48 -0500

    evaluacion1.1

--- app/src/main/java/com/example/evaluacioncontinua1/NumeroMayorActivity.kt
+++ app/src/main/java/com/example/evaluacioncontinua1/NumeroMayorActivity.kt
@@ -1,20 +1,51 @@
 package com.example.evaluacioncontinua1
 
 import android.os.Bundle
+import android.provider.CalendarContract.Instances
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class NumeroMayorActivity : AppCompatActivity() {
+
+    private lateinit var n1: EditText
+    private lateinit var n2: EditText
+    private lateinit var n3: EditText
+    private lateinit var n4: EditText
+    private lateinit var btncalcular: Button
+    private lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_numero_mayor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        n1 = findViewById(R.id.n1)
+        n2 = findViewById(R.id.n2)
+        n3 = findViewById(R.id.n3)
+        n4 = findViewById(R.id.n4)
+        btncalcular = findViewById(R.id.btncalcular)
+        resultado = findViewById(R.id.resultado)
+
+        btncalcular.setOnClickListener {
+            val numeros = listOf(
+                n1.text.toString().toDoubleOrNull(),
+                n2.text.toString().toDoubleOrNull(),
+                n3  .text.toString().toDoubleOrNull(),
+                n4.text.toString().toDoubleOrNull()
+            )
+
+            if (numeros.all { it != null }) {
+                val mayor = numeros.filterNotNull().maxOrNull()
+                resultado.text = "El número mayor es: $mayor"
+            } else {
+                Toast.makeText(this, "Ingrese todos los números correctamente", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
 }
